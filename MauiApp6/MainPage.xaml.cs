22844e4343f78b7c87fd338064eac0f2e0c5e5de
Asus <Asus@DESKTOP-27DJFSC>
2024-10-08 15:46:44 +0300

    Add project files.

---
+++ MauiApp6/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp6
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task OpenMap()
+        {
+            var location = new Location(47.645160, -122.1306032);
+            var options = new MapLaunchOptions { Name = "Microsoft Building 25", NavigationMode = NavigationMode.Driving };
+
+            try
+            {
+                await Map.Default.OpenAsync(location, options);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);// No map application available to open 
+            }
+        }
+        private async void OnOpenMapButtonClicked(object sender, EventArgs e)
+        {
+            await OpenMap(); // Call the method to open maps
+        }
+    }
+    }
+    
+
+
