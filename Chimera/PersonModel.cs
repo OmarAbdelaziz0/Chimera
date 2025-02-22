3f943bacff5ad994e46246c175297e2bbd9d7835
OmarAbdelaziz0 <omar.abdelaziz@qortova.com>
2025-02-22 18:11:12 +0200

    MongoDB connection and Gender and Email restrictions

--- Chimera/PersonModel.cs
+++ Chimera/PersonModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace MauiApp6
 {
@@ -20,7 +21,7 @@ namespace MauiApp6
 
         public int Age { get; set; }
 
-
-
+        public string Email { get; set; }
     }
 }
+
